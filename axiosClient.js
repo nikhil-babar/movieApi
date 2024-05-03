239558f4b7f4077e8e4e56546ed5ab50829b3f3e
Nikhil babar <nikhilbabarjee@gmail.com>
2024-05-03 08:22:56 +0530

    Fixed en

--- axiosClient.js
+++ axiosClient.js
@@ -7,7 +7,7 @@ const agent = new https.Agent({ keepAlive: true });
 module.exports = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "fe5484cb74a458c133c731cccb007eb1",
+    api_key: process.env.API_KEY,
   },
   timeout: 10000,
   httpsAgent: agent,
